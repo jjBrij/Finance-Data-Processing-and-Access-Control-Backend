cbe292a8ca0f88d9a78c0a0a2db58381ec4480d3
B MOHAN PRASAD <brijmohan2204@gmail.com>
2026-04-03 19:58:24 +0530

    add superuser

--- zorvyn/settings.py
+++ zorvyn/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
