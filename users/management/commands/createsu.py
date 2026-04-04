5ba0b5150b02e521ae4cbbe9797d875216ee40da
B MOHAN PRASAD <brijmohan2204@gmail.com>
2026-04-04 11:22:11 +0530

    fix createsu username

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
+        if not User.objects.filter(username='sugandh').exists():
             User.objects.create_superuser(
                 username='sugandh',
                 email='sugandh@sugandh.com',
